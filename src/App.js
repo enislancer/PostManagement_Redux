2650f76f9426145806184b19ccb4b96a199340a0
SuperStarDev51 <v.eridicaldeveloper@gmail.com>
2020-12-26 21:48:20 -0600

    Added postform

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import {
 
 import { Navbar } from './app/Navbar'
 import {PostsList} from './features/posts/PostsList'
+import {AddPostForm} from './features/posts/AddPostForm'
+
 function App() {
   return (
     <Router>
@@ -19,6 +21,7 @@ function App() {
             path="/"
             render={() => (
               <React.Fragment>
+                  <AddPostForm />
                   <PostsList />
               </React.Fragment>
             )}
--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,7 +1,33 @@
-import React, {useStaate} from 'react'
+import React, {useState} from 'react'
 export const AddPostForm = () => {
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
 
-    
+    const onTitleChanged = e => setTitle(e.target.value);
+    const onContentChanged = e => setContent(e.target.value)
+
+    return (
+        <section>
+            <h2>Add a New Post</h2>
+            <form>
+                <label htmlFor="postTitle">Post title:</label>
+                <input 
+                    type = "text"
+                    id = "postTitle"
+                    name = 'posttitle'
+                    value = {title}
+                    onChange = {onTitleChanged}
+                />
+                <label htmlFor="postContent">Content:</label>
+                <textarea
+                    id="postContent"
+                    name="postContent"
+                    value={content}
+                    onChange={onContentChanged}
+                />
+                <button type="button">Save Post</button>
+            </form>
+        </section>
+    )
+
 }
