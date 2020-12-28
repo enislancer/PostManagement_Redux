4d26d86bd5fee5593e9fc9163848e1129170988f
SuperStarDev51 <v.eridicaldeveloper@gmail.com>
2020-12-27 19:59:50 -0600

    editpostform added

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 export const Navbar = () => {
   return (
     <nav>
       <section>
-        <h1>Post management system with Redux</h1>
+        <h1>Redux Essentials Example</h1>
 
         <div className="navContent">
-          <div className="navLinks"></div>
+          <div className="navLinks">
+            <Link to="/">Posts</Link>
+          </div>
         </div>
       </section>
     </nav>
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -10,9 +10,17 @@ const postSlice = createSlice({
     reducers: {
         postAdded(state, action){
             state.push(action.payload)
+        },
+        postUpdated(state, action){
+            const {id, title, content} = action.payload
+            const existingPost = state.find(post => post.id === id)
+            if (existingPost){
+                existingPost.title = title
+                existingPost.content = content
+            }
         }
     }
 })
-export const {postAdded} = postSlice.actions
+export const {postAdded, postUpdated} = postSlice.actions
 export default postSlice.reducer
 
